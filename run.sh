15407463dea5272835d6cd73878d9753421b19dc
scu.wan <scu.wan@qtum.info>
2022-12-15 10:52:40 +0800

    update version

--- run.sh
+++
@@ -1,4 +0,0 @@
-tcp_server=36.189.234.195:10001
-ssl_server=36.189.234.195:10002
-email=test@gmail.com
-./zkwork_aleo_worker --ssl  --verbosity 1 --email $email --tcp_server $tcp_server --ssl_server $ssl_server 2>&1
---
+++ run_solo.sh
@@ -0,0 +1,25 @@
+# server list
+# server hk
+tcp_server=61.10.9.22:30004
+ssl_server=61.10.9.22:30005
+# server us
+#tcp_server=69.194.1.66:30004
+#ssl_server=69.194.1.66:30005
+
+# use your own address instead
+address="aleo1f6ygqhuyx9kyp5rwkze5wdyput82auw3x87r6p36pegqeq2jdu8qwgwsn5"
+
+# try change --cuda_thread --wsize --dsize for best performance
+# The larger the --wsize parameter is in the range supported by gpu memory, the better
+# some suggested values
+#3090
+##cuda_thread=6
+##wsize=13
+#3080
+cuda_thread=3
+wsize=12
+#2080ti
+##cuda_thread=8
+##wsize=12
+
+./solo_prover --address $address --tcp_server $tcp_server --ssl_server $ssl_server 2>&1
